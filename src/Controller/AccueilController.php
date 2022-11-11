7722006e5536bc202f7eb4b57c45c1c10aac08e2
Mika <michael.atlas37@gmail.com>
2022-11-11 15:15:10 +0100

    Ajout page d'accueil

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class AccueilController extends AbstractController
+{
+    #[Route('/', 'accueil.index', methods: ['GET'])]
+    public function index() : Response 
+    {
+        return $this->render('pages/accueil.html.twig');
+    }
+}
